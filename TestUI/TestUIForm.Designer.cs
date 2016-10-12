2a1107b0fe5da9eca0a81b2056f6adf9f4b7c011
tengda <tengda@yiguo.com>
2016-10-12 13:52:07 +0800

    增加测试UI的winform工程

---
+++ TestUI/TestUIForm.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace TestUI
+{
+    partial class TestUIForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.roleA = new System.Windows.Forms.Button();
+            this.roleB = new System.Windows.Forms.Button();
+            this.aWq0 = new System.Windows.Forms.Button();
+            this.aWq1 = new System.Windows.Forms.Button();
+            this.aWq2 = new System.Windows.Forms.Button();
+            this.aWq3 = new System.Windows.Forms.Button();
+            this.bWq3 = new System.Windows.Forms.Button();
+            this.bWq2 = new System.Windows.Forms.Button();
+            this.bWq1 = new System.Windows.Forms.Button();
+            this.bWq0 = new System.Windows.Forms.Button();
+            this.cWq3 = new System.Windows.Forms.Button();
+            this.cWq2 = new System.Windows.Forms.Button();
+            this.cWq1 = new System.Windows.Forms.Button();
+            this.cWq0 = new System.Windows.Forms.Button();
+            this.roleC = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblLifeA = new System.Windows.Forms.Label();
+            this.lblCardNumA = new System.Windows.Forms.Label();
+            this.lblCardNumB = new System.Windows.Forms.Label();
+            this.lblLifeB = new System.Windows.Forms.Label();
+            this.lblCardNumC = new System.Windows.Forms.Label();
+            this.lblLifeC = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnCardTemplate = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // roleA
+            // 
+            this.roleA.Location = new System.Drawing.Point(24, 45);
+            this.roleA.Name = "roleA";
+            this.roleA.Size = new System.Drawing.Size(109, 130);
+            this.roleA.TabIndex = 0;
+            this.roleA.Text = "角色A";
+            this.roleA.UseVisualStyleBackColor = true;
+            // 
+            // roleB
+            // 
+            this.roleB.Location = new System.Drawing.Point(677, 45);
+            this.roleB.Name = "roleB";
+            this.roleB.Size = new System.Drawing.Size(109, 130);
+            this.roleB.TabIndex = 1;
+            this.roleB.Text = "角色B";
+            this.roleB.UseVisualStyleBackColor = true;
+            // 
+            // aWq0
+            // 
+            this.aWq0.Location = new System.Drawing.Point(139, 45);
+            this.aWq0.Name = "aWq0";
+            this.aWq0.Size = new System.Drawing.Size(28, 28);
+            this.aWq0.TabIndex = 2;
+            this.aWq0.UseVisualStyleBackColor = true;
+            // 
+            // aWq1
+            // 
+            this.aWq1.Location = new System.Drawing.Point(139, 79);
+            this.aWq1.Name = "aWq1";
+            this.aWq1.Size = new System.Drawing.Size(28, 28);
+            this.aWq1.TabIndex = 3;
+            this.aWq1.UseVisualStyleBackColor = true;
+            // 
+            // aWq2
+            // 
+            this.aWq2.Location = new System.Drawing.Point(139, 113);
+            this.aWq2.Name = "aWq2";
+            this.aWq2.Size = new System.Drawing.Size(28, 28);
+            this.aWq2.TabIndex = 4;
+            this.aWq2.UseVisualStyleBackColor = true;
+            // 
+            // aWq3
+            // 
+            this.aWq3.Location = new System.Drawing.Point(139, 147);
+            this.aWq3.Name = "aWq3";
+            this.aWq3.Size = new System.Drawing.Size(28, 28);
+            this.aWq3.TabIndex = 5;
+            this.aWq3.UseVisualStyleBackColor = true;
+            // 
+            // bWq3
+            // 
+            this.bWq3.Location = new System.Drawing.Point(643, 147);
+            this.bWq3.Name = "bWq3";
+            this.bWq3.Size = new System.Drawing.Size(28, 28);
+            this.bWq3.TabIndex = 9;
+            this.bWq3.UseVisualStyleBackColor = true;
+            // 
+            // bWq2
+            // 
+            this.bWq2.Location = new System.Drawing.Point(643, 113);
+            this.bWq2.Name = "bWq2";
+            this.bWq2.Size = new System.Drawing.Size(28, 28);
+            this.bWq2.TabIndex = 8;
+            this.bWq2.UseVisualStyleBackColor = true;
+            // 
+            // bWq1
+            // 
+            this.bWq1.Location = new System.Drawing.Point(643, 79);
+            this.bWq1.Name = "bWq1";
+            this.bWq1.Size = new System.Drawing.Size(28, 28);
+            this.bWq1.TabIndex = 7;
+            this.bWq1.UseVisualStyleBackColor = true;
+            // 
+            // bWq0
+            // 
+            this.bWq0.Location = new System.Drawing.Point(643, 45);
+            this.bWq0.Name = "bWq0";
+            this.bWq0.Size = new System.Drawing.Size(28, 28);
+            this.bWq0.TabIndex = 6;
+            this.bWq0.UseVisualStyleBackColor = true;
+            // 
+            // cWq3
+            // 
+            this.cWq3.Location = new System.Drawing.Point(139, 337);
+            this.cWq3.Name = "cWq3";
+            this.cWq3.Size = new System.Drawing.Size(28, 28);
+            this.cWq3.TabIndex = 14;
+            this.cWq3.UseVisualStyleBackColor = true;
+            // 
+            // cWq2
+            // 
+            this.cWq2.Location = new System.Drawing.Point(139, 303);
+            this.cWq2.Name = "cWq2";
+            this.cWq2.Size = new System.Drawing.Size(28, 28);
+            this.cWq2.TabIndex = 13;
+            this.cWq2.UseVisualStyleBackColor = true;
+            // 
+            // cWq1
+            // 
+            this.cWq1.Location = new System.Drawing.Point(139, 269);
+            this.cWq1.Name = "cWq1";
+            this.cWq1.Size = new System.Drawing.Size(28, 28);
+            this.cWq1.TabIndex = 12;
+            this.cWq1.UseVisualStyleBackColor = true;
+            // 
+            // cWq0
+            // 
+            this.cWq0.Location = new System.Drawing.Point(139, 235);
+            this.cWq0.Name = "cWq0";
+            this.cWq0.Size = new System.Drawing.Size(28, 28);
+            this.cWq0.TabIndex = 11;
+            this.cWq0.UseVisualStyleBackColor = true;
+            // 
+            // roleC
+            // 
+            this.roleC.Location = new System.Drawing.Point(24, 235);
+            this.roleC.Name = "roleC";
+            this.roleC.Size = new System.Drawing.Size(109, 130);
+            this.roleC.TabIndex = 10;
+            this.roleC.Text = "角色C";
+            this.roleC.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Location = new System.Drawing.Point(196, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(427, 216);
+            this.panel1.TabIndex = 15;
+            // 
+            // lblLifeA
+            // 
+            this.lblLifeA.AutoSize = true;
+            this.lblLifeA.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblLifeA.Location = new System.Drawing.Point(107, 61);
+            this.lblLifeA.Name = "lblLifeA";
+            this.lblLifeA.Size = new System.Drawing.Size(15, 14);
+            this.lblLifeA.TabIndex = 16;
+            this.lblLifeA.Text = "0";
+            // 
+            // lblCardNumA
+            // 
+            this.lblCardNumA.AutoSize = true;
+            this.lblCardNumA.Location = new System.Drawing.Point(107, 155);
+            this.lblCardNumA.Name = "lblCardNumA";
+            this.lblCardNumA.Size = new System.Drawing.Size(11, 12);
+            this.lblCardNumA.TabIndex = 17;
+            this.lblCardNumA.Text = "0";
+            // 
+            // lblCardNumB
+            // 
+            this.lblCardNumB.AutoSize = true;
+            this.lblCardNumB.Location = new System.Drawing.Point(761, 153);
+            this.lblCardNumB.Name = "lblCardNumB";
+            this.lblCardNumB.Size = new System.Drawing.Size(11, 12);
+            this.lblCardNumB.TabIndex = 19;
+            this.lblCardNumB.Text = "0";
+            // 
+            // lblLifeB
+            // 
+            this.lblLifeB.AutoSize = true;
+            this.lblLifeB.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblLifeB.Location = new System.Drawing.Point(761, 59);
+            this.lblLifeB.Name = "lblLifeB";
+            this.lblLifeB.Size = new System.Drawing.Size(15, 14);
+            this.lblLifeB.TabIndex = 18;
+            this.lblLifeB.Text = "0";
+            // 
+            // lblCardNumC
+            // 
+            this.lblCardNumC.AutoSize = true;
+            this.lblCardNumC.Location = new System.Drawing.Point(107, 343);
+            this.lblCardNumC.Name = "lblCardNumC";
+            this.lblCardNumC.Size = new System.Drawing.Size(11, 12);
+            this.lblCardNumC.TabIndex = 21;
+            this.lblCardNumC.Text = "0";
+            // 
+            // lblLifeC
+            // 
+            this.lblLifeC.AutoSize = true;
+            this.lblLifeC.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblLifeC.Location = new System.Drawing.Point(107, 249);
+            this.lblLifeC.Name = "lblLifeC";
+            this.lblLifeC.Size = new System.Drawing.Size(15, 14);
+            this.lblLifeC.TabIndex = 20;
+            this.lblLifeC.Text = "0";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnCardTemplate);
+            this.panel2.Location = new System.Drawing.Point(196, 265);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(427, 100);
+            this.panel2.TabIndex = 22;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(654, 235);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(122, 130);
+            this.panel3.TabIndex = 23;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 101);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(399, 104);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnCardTemplate
+            // 
+            this.btnCardTemplate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnCardTemplate.ForeColor = System.Drawing.Color.Coral;
+            this.btnCardTemplate.Location = new System.Drawing.Point(3, 11);
+            this.btnCardTemplate.Name = "btnCardTemplate";
+            this.btnCardTemplate.Size = new System.Drawing.Size(67, 82);
+            this.btnCardTemplate.TabIndex = 0;
+            this.btnCardTemplate.Text = "红桃A";
+            this.btnCardTemplate.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 412);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.lblCardNumC);
+            this.Controls.Add(this.lblLifeC);
+            this.Controls.Add(this.lblCardNumB);
+            this.Controls.Add(this.lblLifeB);
+            this.Controls.Add(this.lblCardNumA);
+            this.Controls.Add(this.lblLifeA);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cWq3);
+            this.Controls.Add(this.cWq2);
+            this.Controls.Add(this.cWq1);
+            this.Controls.Add(this.cWq0);
+            this.Controls.Add(this.roleC);
+            this.Controls.Add(this.bWq3);
+            this.Controls.Add(this.bWq2);
+            this.Controls.Add(this.bWq1);
+            this.Controls.Add(this.bWq0);
+            this.Controls.Add(this.aWq3);
+            this.Controls.Add(this.aWq2);
+            this.Controls.Add(this.aWq1);
+            this.Controls.Add(this.aWq0);
+            this.Controls.Add(this.roleB);
+            this.Controls.Add(this.roleA);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button roleA;
+        private System.Windows.Forms.Button roleB;
+        private System.Windows.Forms.Button aWq0;
+        private System.Windows.Forms.Button aWq1;
+        private System.Windows.Forms.Button aWq2;
+        private System.Windows.Forms.Button aWq3;
+        private System.Windows.Forms.Button bWq3;
+        private System.Windows.Forms.Button bWq2;
+        private System.Windows.Forms.Button bWq1;
+        private System.Windows.Forms.Button bWq0;
+        private System.Windows.Forms.Button cWq3;
+        private System.Windows.Forms.Button cWq2;
+        private System.Windows.Forms.Button cWq1;
+        private System.Windows.Forms.Button cWq0;
+        private System.Windows.Forms.Button roleC;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblLifeA;
+        private System.Windows.Forms.Label lblCardNumA;
+        private System.Windows.Forms.Label lblCardNumB;
+        private System.Windows.Forms.Label lblLifeB;
+        private System.Windows.Forms.Label lblCardNumC;
+        private System.Windows.Forms.Label lblLifeC;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnCardTemplate;
+    }
+}
+
